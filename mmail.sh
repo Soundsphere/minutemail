3880b838ebb4988d93c58493169452c4d9cb5a6f
Soundsphere <benedikt.rumpf@gmail.com>
2016-11-27 12:02:20 +0100

    First Commit
    Upload of all the files

---
+++ mmail.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+#
+# MinuteMail script created by Benedikt Rumpf, System Administrator at
+# Caroobi GmbH
+# This script sends out a MinuteMail to 1mm@caroobi.com, product@caroobi.com
+# and noida.tech@caroobi.com
+# It will be constantly updated and improved. Should you be able to script
+# in bash and come across this file, please feel free improve and or otherwise
+# use this script.
+# Before using this program, have a look at
+# https://wiki.archlinux.org/index.php/SSMTP in order to install the mailer
+#
+###############################################################################
+# Bug seems to be fixed, keep an eye on how it performs in the wild
+###############################################################################
+
+# Path to files
+MM="$HOME/bin/mm"
+
+# recipients of the email
+#DEST="1mm@caroobi.com,product@caroobi.com,noida.tech@caroobi.com"
+# uncomment for debugging:
+DEST="benedikt.rumpf@gmail.com"
+
+# todays date. Format: Weekday, DD Month YYYY
+NOW=$(date +%A,\ %d.\ %B\ %Y)
+
+# combine all files to one
+function catall() {
+  cat $MM/Header $MM/Worked $MM/Roadblocks $MM/Observations $MM/Thoughts $MM/Feelings $MM/Footer > $MM/MinuteMail
+}
+
+# reset all the files after send
+function reset() {
+  printf "Worked on:" > $MM/Worked
+  printf "\n\nFeelings:" > $MM/Feelings
+  printf "\n\nRoadblocks:" > $MM/Roadblocks
+  printf "\n\nObservations:" > $MM/Observations
+  printf "\n\nThoughts:" > $MM/Thoughts
+  if [ -f "$MM/MinuteMail" ]; then
+    rm $MM/MinuteMail
+    echo "mm reset"
+  else
+    echo "Nothing to do, mm already reset"
+  fi
+}
+
+# send the MinuteMail
+function sendmm() {
+  cat $MM/MinuteMail | mail -s "MinuteMail Benedikt - $NOW" $DEST
+  echo "mail sent"
+  reset
+  touch $MM/sent
+}
+
+# program begins here
+case $1 in
+  w)      printf "\n - $2" >> $MM/Worked
+  ;;
+  r)      printf "\n - $2" >> $MM/Roadblocks
+  ;;
+  o)      printf "\n - $2" >> $MM/Observations
+  ;;
+  t)      printf "\n - $2" >> $MM/Thoughts
+  ;;
+  f)      printf "\n - $2" >> $MM/Feelings
+  ;;
+  check)  catall
+          cat $MM/MinuteMail
+  ;;
+  done)   catall
+          cat $MM/MinuteMail
+          printf "\n\n"
+          read -p "Edit (e) or send (s) MinuteMail? " edit
+          case $edit in
+            e)  nano $MM/MinuteMail
+                printf "\n\n"
+                read -p "Send now? " send
+                case $send in
+                  y) sendmm
+                  ;;
+                  *)  echo "not sent"
+                esac
+            ;;
+            s)  sendmm
+            ;;
+            *) echo "not sent"
+          esac
+  ;;
+  send)   sendmm
+  ;;
+  # this section is called from the reminder script
+  # it opens an xterm and closes it right after the interaction
+  remind) catall
+          cat $MM/MinuteMail
+          printf "\n\n"
+          read -p "Edit (e) or send (s) MinuteMail? " edit
+          case $edit in
+            e)  nano $MM/MinuteMail
+                printf "\n\n"
+                read -p "Send now? " send
+                case $send in
+                  y)  sendmm
+                      exit
+                  ;;
+                  *)  echo "not sent"
+                esac
+            ;;
+            s)  sendmm
+                exit
+            ;;
+            *) echo "not sent"
+          esac
+  ;;
+  help)   cat $MM/Helpfile
+  ;;
+  status) catall
+          cat $MM/MinuteMail
+  ;;
+  reset)  reset
+  ;;
+  test)   catall
+          cat $MM/MinuteMail | mail -s "MinuteMail Benedikt - $NOW" benedikt.rumpf@caroobi.com
+          touch $MM/sent
+          echo "mail sent, but not reset"
+  ;;
+  *)      echo "Invalid or no option given. Please see mm help for further info"
+esac
---
+++ mmreminder.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+#
+# This is the reminder for the MinuteMail
+# It will be called via cron every day at 18:30
+
+# specify display for cron
+export DISPLAY=:0.0
+
+# in order to use notify-send, this needs to be here to call the script
+# in .dbus
+if [ -r "$HOME/.dbus/Xdbus" ]; then
+  . "$HOME/.dbus/Xdbus"
+fi
+
+# run the actual thing
+if [ ! -f "/home/bene/bin/mm/sent" ]; then
+  /usr/bin/notify-send -t 1000 -i /usr/share/icons/Vibrancy-Colors/apps/256/email.png MinuteMail "Time to send your MinuteMail"
+  sleep 1
+  xterm -geometry 100x55 -e /home/bene/bin/./mmail.sh remind
+else
+  rm /home/bene/bin/mm/sent
+fi
