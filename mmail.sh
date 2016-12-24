9b10e1b90d578731ffb18b46acffa5513505f0a3
Soundsphere <benedikt.rumpf@gmail.com>
2016-12-24 16:56:06 +0100

    Variable update
    Changed how to set the variable for the working directory. This is now in the .conf file

--- mmail.sh
+++ mmail.sh
@@ -2,7 +2,7 @@
 #
 # MinuteMail script created by Benedikt Rumpf, System Administrator at
 # Caroobi GmbH
-# This script sends out a MinuteMail our managers and teammates.
+# This script sends out a MinuteMail to our managers.
 # It will be constantly updated and improved. Should you be able to script
 # in bash and come across this file, please feel free improve and or otherwise
 # use this script.
@@ -10,30 +10,27 @@
 # https://wiki.archlinux.org/index.php/SSMTP in order to install the mailer
 # have a look at getopt for better case handling.
 
-# Path to files
-MM="$HOME/bin/mm"
-
 # Source mm.conf
-# Set your recipients in this config file
-source $HOME/bin/mm.conf
+# The recipients and the working directory are set in this file
+source ./mm.conf
 
 # todays date. Format: Weekday, DD Month YYYY
 NOW=$(date +%A,\ %d.\ %B\ %Y)
 
 # combine all files to one
 function catall() {
-  cat $MM/{Header,Worked,Roadblocks,Observations,Thoughts,Feelings,Footer} > "$MM/MinuteMail"
+  cat $mm_dir/{Header,Worked,Roadblocks,Observations,Thoughts,Feelings,Footer} > "$mm_dir/MinuteMail"
 }
 
 # reset all the files after send
 function reset() {
-  printf "\n\nWorked on:" > "$MM/Worked"
-  printf "\n\nFeelings:" > "$MM/Feelings"
-  printf "\n\nRoadblocks:" > "$MM/Roadblocks"
-  printf "\n\nObservations:" > "$MM/Observations"
-  printf "\n\nThoughts:" > "$MM/Thoughts"
-  if [ -f "$MM/MinuteMail" ]; then
-    rm "$MM/MinuteMail"
+  printf "\nWorked on:" > "$mm_dir/Worked"
+  printf "\n\nFeelings:" > "$mm_dir/Feelings"
+  printf "\n\nRoadblocks:" > "$mm_dir/Roadblocks"
+  printf "\n\nObservations:" > "$mm_dir/Observations"
+  printf "\n\nThoughts:" > "$mm_dir/Thoughts"
+  if [ -f "$mm_dir/MinuteMail" ]; then
+    rm "$mm_dir/MinuteMail"
     echo "mm reset"
   else
     echo "Nothing to do, mm already reset"
@@ -44,33 +41,33 @@ function reset() {
 # the file "sent" gets created for the cronjob. If the file does not exist,
 # run the cron to notify me of the MinuteMail that I have to send
 function sendmm() {
-  mail -s "MinuteMail Benedikt - $NOW" $mm_recipients < "$MM/MinuteMail"
+  mail -s "MinuteMail Benedikt - $NOW" $mm_dir_recipients < "$mm_dir/MinuteMail"
   echo "mail sent"
   reset
-  touch "$MM/sent"
+  touch "$mm_dir/sent"
 }
 
 # program begins here
 case $1 in
-  w)      printf "\n - %b" "$2" >> "$MM/Worked"
+  w)      printf "\n - %b" "$2" >> "$mm_dir/Worked"
   ;;
-  r)      printf "\n - %b" "$2" >> "$MM/Roadblocks"
+  r)      printf "\n - %b" "$2" >> "$mm_dir/Roadblocks"
   ;;
-  o)      printf "\n - %b" "$2" >> "$MM/Observations"
+  o)      printf "\n - %b" "$2" >> "$mm_dir/Observations"
   ;;
-  t)      printf "\n - %b" "$2" >> "$MM/Thoughts"
+  t)      printf "\n - %b" "$2" >> "$mm_dir/Thoughts"
   ;;
-  f)      printf "\n - %b" "$2" >> "$MM/Feelings"
+  f)      printf "\n - %b" "$2" >> "$mm_dir/Feelings"
   ;;
   c)      catall
-          cat "$MM/MinuteMail"
+          cat "$mm_dir/MinuteMail"
   ;;
   d)      catall
-          cat "$MM/MinuteMail"
+          cat "$mm_dir/MinuteMail"
           printf "\n\n"
           read -p "Edit (e) or send (s) MinuteMail? " edit
           case $edit in
-            e)  nano "$MM/MinuteMail"
+            e)  nano "$mm_dir/MinuteMail"
                 printf "\n\n"
                 read -p "Send now (s)?" send
                 case $send in
@@ -90,11 +87,11 @@ case $1 in
   # this section is called from the reminder script
   # it opens an xterm and closes it right after the interaction
   remind) catall
-          cat "$MM/MinuteMail"
+          cat "$mm_dir/MinuteMail"
           printf "\n\n"
           read -p "Edit (e) or send (s) MinuteMail? " edit
           case $edit in
-            e)  nano "$MM/MinuteMail"
+            e)  nano "$mm_dir/MinuteMail"
                 printf "\n\n"
                 read -p "Send now (s)?" send
                 case $send in
@@ -110,13 +107,13 @@ case $1 in
             *) echo "not sent"
           esac
   ;;
-  help)   cat "$MM/Helpfile"
+  help)   cat "$mm_dir/Helpfile"
   ;;
   reset)  reset
   ;;
   test)   catall
-          mail -s "MinuteMail Benedikt - $NOW" $mm_debug < "$MM/MinuteMail"
-          touch "$MM/sent"
+          mail -s "MinuteMail Benedikt - $NOW" $mm_dir_debug < "$mm_dir/MinuteMail"
+          touch "$mm_dir/sent"
           echo "mail sent, but not reset"
   ;;
   *)      echo "Invalid or no option given. Please see mm help for further info"
